cad6a5bf3da0dae8b4ff9667941c5cdb27f3a1a3
akshayaps1983 <110896904+akshayaps1983@users.noreply.github.com>
2022-08-09 15:26:42 +0530

    first

---
+++ forloop.py
@@ -0,0 +1,10 @@
+for_list=[1,2,3,4,5,67,8,9,10]
+for x in for_list:
+    print(x)
+    print(x*x)
+    print(x*x*x)
+    print(x*x*x*x)
+    for_list=[["akku",23],["achu",21],["appu",56],["ammu",45]]
+    for i in for_list:
+        print(i[0],"good girl")
+        print(i[1],"happy girls")
---
+++ forloopbasics.py
@@ -0,0 +1,8 @@
+list={1,2,3,4,5,6,7,8,9,10}
+for i in list:
+    print(i)
+
+num=10
+if num%2\
+        ==0:
+        print("num is divisible by 2")
