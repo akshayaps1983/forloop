4a2c59ae5bf684c68e834944c4fa8c97680f9944
akshayaps1983 <110896904+akshayaps1983@users.noreply.github.com>
2022-08-09 15:17:23 +0530

    first

---
+++ nested.py
@@ -0,0 +1,23 @@
+nested_dictionary={
+    "one":
+    {
+    "name":"john",
+    "age":24,
+"city":"New york"
+},
+    "two":
+        {
+            "name":"Jane",
+            "age":24,
+            "city":"paris"
+        },
+    "three":
+        {
+            "name":"Bobby",
+            "age":20,
+            "city":"London"
+
+
+        }
+}
+print(nested_dictionary["three"]["name"])
